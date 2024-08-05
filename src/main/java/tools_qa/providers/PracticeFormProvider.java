e7b7ff1671044241d5e0b4a8b27ae618804be215
bartlomiejfydrych <fydbar@gmail.com>
2024-08-05 20:16:36 +0200

    05.08.2024 - Tworzenie buildera dla PracticeFormPage

--- src/main/java/tools_qa/providers/PracticeFormProvider.java
+++ src/main/java/tools_qa/providers/PracticeFormProvider.java
@@ -3,9 +3,12 @@ package tools_qa.providers;
 import com.github.javafaker.Faker;
 import tools_qa.models.PracticeForm;
 
-public class PracticeFormProvider {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
 
-    Faker faker = new Faker();
+public class PracticeFormProvider {
 
     public static PracticeForm getRandomPracticeForm() {
         Faker faker = new Faker();
@@ -14,18 +17,78 @@ public class PracticeFormProvider {
                 .lastName(faker.name().lastName())
                 .email(faker.internet().emailAddress())
                 .mobileNumber(faker.phoneNumber().phoneNumber())
+                .currentAddress(getRandomCurrentAddress())
 
+
+                .build();
     }
 
     // -------
     // METHODS
     // -------
 
-    public void getRandomCurrentAddress() {
-        String
+    public static String getRandomCurrentAddress() {
+        Faker faker = new Faker();
+        String country = faker.address().country();
+        String city = faker.address().city();
+        String zipCode = faker.address().zipCode();
+        String streetName = faker.address().streetName();
+        String streetNumber = faker.address().streetAddressNumber();
+        String buildingNumber = faker.address().buildingNumber();
+        String currentAddress = country + "\n"
+                + city + "\n"
+                + zipCode + "\n"
+                + streetName + " " + streetNumber + "/" + buildingNumber;
+        return currentAddress;
+    }
+
+    public String getRandomState() {
+        Random random = new Random();
+        String randomState = stateList.get(random.nextInt(stateList.size()));
+        return randomState;
+    }
+
+    public String getRandomCity(String state) {
+        switch(state) {
+            case "NCR":
+
+        }
     }
 
     // ----
     // DATA
     // ----
+
+    // State
+
+    List<String> stateList = Arrays.asList(
+            "NCR",
+            "Uttar Pradesh",
+            "Haryana",
+            "Rajasthan"
+    );
+
+    // City
+
+    List<String> cityNcrList = Arrays.asList(
+            "Delhi",
+            "Gurgaon",
+            "Noida"
+    );
+
+    List<String> cityUttarPradeshList = Arrays.asList(
+            "Agra",
+            "Lucknow",
+            "Merrut"
+    );
+
+    List<String> cityHaryanaList = Arrays.asList(
+            "Karnal",
+            "Panipat"
+    );
+
+    List<String> cityRajasthanList = Arrays.asList(
+            "Jaipur",
+            "Jaiselmer"
+    );
 }
