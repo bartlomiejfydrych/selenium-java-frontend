df408e6c4a070132040f54fbd90c38a24036fce3
bartlomiejfydrych <fydbar@gmail.com>
2024-11-07 17:38:42 +0100

    07.11.2024 - Dodanie testów dla Dragabble Page

--- src/test/java/tools_qa/interactions_tests/DragabbleTest.java
+++ src/test/java/tools_qa/interactions_tests/DragabbleTest.java
@@ -11,7 +11,7 @@ import tools_qa.pages.normal.interactions_pages.InteractionsPage;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class DragabbleTest  extends TestBase {
+public class DragabbleTest extends TestBase {
 
     // NOTE: The creators made a mistake in the name of this sub-page. It should be "Draggable"
 
@@ -33,7 +33,7 @@ public class DragabbleTest  extends TestBase {
     // SIMPLE
 
     @Test
-    public void shouldDragElementToMyLocation() {
+    public void shouldDragSimpleElementToSetLocation() {
 
         // -------
         // ARRANGE
@@ -61,5 +61,145 @@ public class DragabbleTest  extends TestBase {
 
         assertThat(dragBox.getCssValue("left")).isEqualTo(expectedLeftPosition);
         assertThat(dragBox.getCssValue("top")).isEqualTo(expectedTopPosition);
+    }
+
+    // AXIS RESTRICTED
+
+    @Test
+    public void shouldDragAxisElementXAndYToSetLocation() {
+
+        // -------
+        // ARRANGE
+        // -------
+
+        // BOX X
+        WebElement dragBoxX = dragabblePage.getAxisOnlyXBox();
+        String expectedBoxXLeftPosition = "573px";
+        String expectedBoxXTopPosition = "0px";
+        // BOX Y
+        WebElement dragBoxY = dragabblePage.getAxisOnlyYBox();
+        String expectedBoxYLeftPosition = "0px";
+        String expectedBoxYTopPosition = "-95px";
+
+        // ---
+        // ACT
+        // ---
+
+        homePage.goToInteractionsPage();
+
+        trainingPage.removeFooterAndAds();
+
+        interactionsPage.goToDragabblePage();
+
+        dragabblePage.clickAxisRestrictedTab()
+                .setPositionLeft(dragBoxX, expectedBoxXLeftPosition)
+                .setPositionTop(dragBoxY, expectedBoxYTopPosition);
+
+        // ------
+        // ASSERT
+        // ------
+
+        // BOX X
+        assertThat(dragBoxX.getCssValue("left")).isEqualTo(expectedBoxXLeftPosition);
+        assertThat(dragBoxX.getCssValue("top")).isEqualTo(expectedBoxXTopPosition);
+        // BOX Y
+        assertThat(dragBoxY.getCssValue("left")).isEqualTo(expectedBoxYLeftPosition);
+        assertThat(dragBoxY.getCssValue("top")).isEqualTo(expectedBoxYTopPosition);
+    }
+
+    // CONTAINER RESTRICTED
+
+    @Test
+    public void shouldDragContainerElementsToSetLocation() {
+
+        // -------
+        // ARRANGE
+        // -------
+
+        // Box in container
+        WebElement dragBoxInContainer = dragabblePage.getContainerBoxInContainer();
+        String expectedBoxInContainerLeftPosition = "673px";
+        String expectedBoxInContainerTopPosition = "106px";
+        // Text in container
+        WebElement dragTextInContainer = dragabblePage.getContainerTextInContainer();
+        String expectedTextInContainerLeftPosition = "13px";
+        String expectedTextInContainerTopPosition = "86px";
+
+        // ---
+        // ACT
+        // ---
+
+        homePage.goToInteractionsPage();
+
+        trainingPage.removeFooterAndAds();
+
+        interactionsPage.goToDragabblePage();
+
+        dragabblePage.clickContainerRestrictedTab()
+                .setPositionLeftAndTop(dragBoxInContainer, expectedBoxInContainerLeftPosition, expectedBoxInContainerTopPosition)
+                .setPositionLeftAndTop(dragTextInContainer, expectedTextInContainerLeftPosition, expectedTextInContainerTopPosition);
+
+        // ------
+        // ASSERT
+        // ------
+
+        // Box in container
+        assertThat(dragBoxInContainer.getCssValue("left")).isEqualTo(expectedBoxInContainerLeftPosition);
+        assertThat(dragBoxInContainer.getCssValue("top")).isEqualTo(expectedBoxInContainerTopPosition);
+        // Text in container
+        assertThat(dragTextInContainer.getCssValue("left")).isEqualTo(expectedTextInContainerLeftPosition);
+        assertThat(dragTextInContainer.getCssValue("top")).isEqualTo(expectedTextInContainerTopPosition);
+    }
+
+    // CURSOR STYLE
+
+    @Test
+    public void shouldDragCursorElementsToSetLocation() {
+
+        // -------
+        // ARRANGE
+        // -------
+
+        // Top left box
+        WebElement dragTopLeftBox = dragabblePage.getCursorTopLeftBox();
+        String expectedTopLeftBoxLeftPosition = "784.25px";
+        String expectedTopLeftBoxTopPosition = "-130px";
+        // Center box
+        WebElement dragCenterBox = dragabblePage.getCursorCenterBox();
+        String expectedCenterBoxLeftPosition = "784.25px";
+        String expectedCenterBoxTopPosition = "100px";
+        // Bottom box
+        WebElement dragBottomBox = dragabblePage.getCursorBottomBox();
+        String expectedBottomBoxLeftPosition = "784.25px";
+        String expectedBottomBoxTopPosition = "-20px";
+
+        // ---
+        // ACT
+        // ---
+
+        homePage.goToInteractionsPage();
+
+        trainingPage.removeFooterAndAds();
+
+        interactionsPage.goToDragabblePage();
+
+        dragabblePage.clickCursorStyleTab()
+                .setPositionLeftAndTop(dragTopLeftBox, expectedTopLeftBoxLeftPosition, expectedTopLeftBoxTopPosition)
+                .setPositionLeftAndTop(dragCenterBox, expectedCenterBoxLeftPosition, expectedCenterBoxTopPosition)
+                .setPositionLeftAndTop(dragBottomBox, expectedBottomBoxLeftPosition, expectedBottomBoxTopPosition);
+
+        // ------
+        // ASSERT
+        // ------
+
+        // Top left box
+        assertThat(dragTopLeftBox.getCssValue("left")).isEqualTo(expectedTopLeftBoxLeftPosition);
+        assertThat(dragTopLeftBox.getCssValue("top")).isEqualTo(expectedTopLeftBoxTopPosition);
+        // Center box
+        assertThat(dragCenterBox.getCssValue("left")).isEqualTo(expectedCenterBoxLeftPosition);
+        assertThat(dragCenterBox.getCssValue("top")).isEqualTo(expectedCenterBoxTopPosition);
+        // Bottom box
+        assertThat(dragBottomBox.getCssValue("left")).isEqualTo(expectedBottomBoxLeftPosition);
+        assertThat(dragBottomBox.getCssValue("top")).isEqualTo(expectedBottomBoxTopPosition);
     }
 }
